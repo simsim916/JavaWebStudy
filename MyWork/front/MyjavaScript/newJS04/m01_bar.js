c5b3bd9289eeeafe3836b5d01b78f2af6c2feb9c
moon_serface <simsim916@naver.com>
2024-03-07 18:39:36 +0900

    24-03-07

---
+++ MyWork/front/MyjavaScript/newJS04/m01_bar.js
@@ -0,0 +1,5 @@
+var menu = '짜장';
+console.log(`** bar.js let menu=${menu}`);
+console.log(`** bar.js let menu=${window.menu}`); // undefined
+//var menu='짜장';
+//console.log(`** bar.js var menu=${window.menu}`);
---
+++ MyWork/front/MyjavaScript/newJS04/m01_foo.js
@@ -0,0 +1,5 @@
+// let menu='짬뽕';
+console.log(`** foo.js: let menu=${menu}`);
+console.log(`** foo.js: let menu=${window.menu}`); // undefined
+//var menu='짬뽕';
+//console.log(`** foo.js: var menu=${window.menu}`);
